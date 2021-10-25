c3a12350c3bc701ab54a660a85eabcefe5bf7cc3
Zechariah <zechariahtan144@gmail.com>
2021-10-26 00:28:44 +0800

    Save theme when reload page

--- src/reducers/ThemeReducer.tsx
+++ src/reducers/ThemeReducer.tsx
@@ -1,11 +1,13 @@
 import { Reducer } from "redux"
 import { iThemeActions, iThemeData } from "../redux"
 
-const initialState: iThemeData = "dark"
+const theme = localStorage.getItem("theme") || ""
+const initialState: iThemeData = ["light", "dark"].includes(theme) ? (theme as "light" | "dark") : "dark"
 
 const reducer: Reducer<iThemeData, iThemeActions> = (state = initialState, action: iThemeActions) => {
 	switch (action.type) {
 		case "SET_THEME":
+			localStorage.setItem("theme", action.payload.theme)
 			return action.payload.theme
 		default:
 			return state
