152ebd818c9b84ec1031777bb32281e0d04ba073
Zechariah <zechariahtan144@gmail.com>
2021-12-14 21:38:08 +0800

    Built Artists page with shitty Appearances UI

---
+++ src/components/ArtistAppearances.tsx
@@ -0,0 +1,181 @@
+import React, { useEffect, useState } from "react"
+import useSpotifyApi from "../hooks/useSpotifyApi"
+import { Card, CardContent, Stack, Typography } from "@mui/material"
+import { set_error } from "../actions/ErrorActions"
+import { useDispatch, useSelector } from "react-redux"
+
+interface Props {
+	artist?: SpotifyApi.ArtistObjectFull
+}
+
+const ArtistAppearances = (props: Props): JSX.Element => {
+	const { artist } = props
+
+	//#region Hooks
+	const dispatch = useDispatch()
+	const api = useSpotifyApi()
+	const statistics = useSelector(state => state.statistics)
+	const [following, setFollowing] = useState<boolean>()
+	const [shortTermArtistsIndex, setShortTermArtistsIndex] = useState(0)
+	const [mediumTermArtistsIndex, setMediumTermArtistsIndex] = useState(0)
+	const [longTermArtistsIndex, setLongTermArtistsIndex] = useState(0)
+	const [shortTermTracksCount, setShortTermTracksCount] = useState(0)
+	const [mediumTermTracksCount, setMediumTermTracksCount] = useState(0)
+	const [longTermTracksCount, setLongTermTracksCount] = useState(0)
+	const [recentArtistsCount, setRecentArtistsCount] = useState(0)
+	//#endregion
+
+	//#region Effects
+	useEffect(() => {
+		if (!api) return
+		if (!artist) return
+
+		api.isFollowingArtists([artist.id])
+			.then(res => setFollowing(res[0]))
+			.catch(err => {
+				dispatch(
+					set_error(err.message === "invalid id" ? new Error("Artist not found") : err)
+				)
+			})
+	}, [dispatch, api, artist])
+
+	useEffect(() => {
+		setShortTermArtistsIndex(
+			(statistics.artists.short_term
+				? statistics.artists.short_term.findIndex(t => t.id === artist?.id)
+				: -1) + 1
+		)
+		setMediumTermArtistsIndex(
+			(statistics.artists.medium_term
+				? statistics.artists.medium_term.findIndex(t => t.id === artist?.id)
+				: -1) + 1
+		)
+		setLongTermArtistsIndex(
+			(statistics.artists.long_term
+				? statistics.artists.long_term.findIndex(t => t.id === artist?.id)
+				: -1) + 1
+		)
+		setShortTermTracksCount(
+			statistics.tracks.short_term?.filter(t => t.artists.find(a => a.id === artist?.id))
+				.length || 0
+		)
+		setMediumTermTracksCount(
+			statistics.tracks.medium_term?.filter(t => t.artists.find(a => a.id === artist?.id))
+				.length || 0
+		)
+		setLongTermTracksCount(
+			statistics.tracks.long_term?.filter(t => t.artists.find(a => a.id === artist?.id))
+				.length || 0
+		)
+		setRecentArtistsCount(
+			statistics.recents?.filter(t => t.track.artists.find(a => a.id === artist?.id))
+				.length || 0
+		)
+	}, [artist, statistics])
+	//#endregion
+
+	return artist ? (
+		<Stack sx={{ width: "fit-content", m: "auto", mt: 3 }}>
+			{following ? (
+				<Card sx={{ mb: 3 }}>
+					<CardContent>
+						<Typography color="primary.main" variant="h6">
+							You are following {artist.name}
+						</Typography>
+					</CardContent>
+				</Card>
+			) : null}
+			{shortTermArtistsIndex ? (
+				<Card sx={{ mb: 3 }}>
+					<CardContent>
+						<Typography color="primary.main" variant="h3">
+							#{shortTermArtistsIndex}
+						</Typography>
+						<Typography variant="h6">
+							of your most streamed artists in the last 4 weeks
+						</Typography>
+					</CardContent>
+				</Card>
+			) : null}
+			{mediumTermArtistsIndex ? (
+				<Card sx={{ mb: 3 }}>
+					<CardContent>
+						<Typography color="primary.main" variant="h3">
+							#{mediumTermArtistsIndex}
+						</Typography>
+						<Typography variant="h6">
+							of your most streamed artists in the last 6 months
+						</Typography>
+					</CardContent>
+				</Card>
+			) : null}
+			{longTermArtistsIndex ? (
+				<Card sx={{ mb: 3 }}>
+					<CardContent>
+						<Typography color="primary.main" variant="h3">
+							#{longTermArtistsIndex}
+						</Typography>
+						<Typography variant="h6">
+							of your most streamed artists in your lifetime
+						</Typography>
+					</CardContent>
+				</Card>
+			) : null}
+			{shortTermTracksCount ? (
+				<Card sx={{ mb: 3 }}>
+					<CardContent>
+						<Typography color="primary.main" variant="h3">
+							{shortTermTracksCount}
+						</Typography>
+						<Typography variant="h6">
+							appearances from {artist.name} in your most streamed tracks in the last 4
+							weeks
+						</Typography>
+					</CardContent>
+				</Card>
+			) : null}
+			{mediumTermTracksCount ? (
+				<Card sx={{ mb: 3 }}>
+					<CardContent>
+						<Typography color="primary.main" variant="h3">
+							{mediumTermTracksCount}
+						</Typography>
+						<Typography variant="h6">
+							appearances from {artist.name} in your most streamed tracks in the last 6
+							months
+						</Typography>
+					</CardContent>
+				</Card>
+			) : null}
+			{longTermTracksCount ? (
+				<Card sx={{ mb: 3 }}>
+					<CardContent>
+						<Typography color="primary.main" variant="h3">
+							{longTermTracksCount}
+						</Typography>
+						<Typography variant="h6">
+							appearances from {artist.name} in your most streamed tracks in your
+							lifetime
+						</Typography>
+					</CardContent>
+				</Card>
+			) : null}
+			{recentArtistsCount ? (
+				<Card sx={{ mb: 3 }}>
+					<CardContent>
+						<Typography color="primary.main" variant="h3">
+							{recentArtistsCount}
+						</Typography>
+						<Typography variant="h6">
+							appearances from {artist.name} in your last 50 streams
+						</Typography>
+					</CardContent>
+				</Card>
+			) : null}
+		</Stack>
+	) : (
+		<></>
+	)
+}
+
+export default ArtistAppearances
--- src/pages/Artist.tsx
+++ src/pages/Artist.tsx
@@ -1,7 +1,189 @@
-import React from "react"
+import ArtistAppearances from "../components/ArtistAppearances"
+import getFollowers from "../utils/getFollowers"
+import React, { useEffect, useState } from "react"
+import Recommendations from "../components/Recommendations"
+import useAuthenticated from "../hooks/useAthenticated"
+import useSpotifyApi from "../hooks/useSpotifyApi"
+import { Avatar, Backdrop, Box, Card, CardActionArea, CardMedia, Container, Dialog, Grid, IconButton, List, ListItem, ListItemAvatar, ListItemText, Skeleton, Tooltip, Typography } from "@mui/material"
+import { set_error } from "../actions/ErrorActions"
+import { useDispatch } from "react-redux"
+import { useHistory, useLocation } from "react-router-dom"
+
+/**
+ * * Name
+ * * Followers
+ * * Image
+ * * Link
+ * * Top Tracks
+ * * Check if you are following this
+ * * Position in Top Artists
+ * * Appearances in Top Tracks
+ * * Appearances in Recents
+ * ? Recommended
+ */
 
 const Artist = (): JSX.Element => {
-	return <></>
+	//#region Hooks
+	const dispatch = useDispatch()
+	const history = useHistory()
+	const location = useLocation()
+	const api = useSpotifyApi()
+	const [showImage, setShowImage] = useState(false)
+	const [artist, setArtist] = useState<SpotifyApi.SingleArtistResponse | null>()
+	const [topTracks, setTopTracks] = useState<SpotifyApi.TrackObjectFull[]>([])
+	//#endregion
+
+	//#region Effects
+	useAuthenticated()
+
+	useEffect(() => {
+		if (!api) return
+
+		const [, , artistId] = location.pathname.split("/")
+		if (artistId) {
+			api.getArtist(artistId)
+				.then(artist => {
+					setArtist(artist)
+					return api.getArtistTopTracks(artist.id, "SG")
+				})
+				.then(res => setTopTracks(res.tracks.slice(0, 5)))
+				.catch(err => {
+					console.error(err)
+					setArtist(null)
+					dispatch(
+						set_error(
+							err.message === "invalid id" ? new Error("Artist not found") : err
+						)
+					)
+				})
+		} else {
+			dispatch(set_error(new Error("Artist not found")))
+		}
+	}, [dispatch, location, api])
+	//#endregion
+
+	//#region Functions
+	const handleArtistOpen = () => {
+		if (artist) {
+			window.open(artist.external_urls.spotify)
+		}
+	}
+
+	const handleTrackClick = (track: SpotifyApi.TrackObjectFull) => {
+		history.push("/track/" + track.id)
+	}
+
+	const handleImageOpen = () => {
+		setShowImage(true)
+	}
+
+	const handleImageClose = () => {
+		setShowImage(false)
+	}
+	//#endregion
+
+	return (
+		<>
+			<Container>
+				<Grid
+					sx={{ mt: { xs: 2, sm: 4 }, mb: { sm: 4 } }}
+					container
+					direction={{ xs: "column", sm: "row" }}>
+					<Grid sx={{ mx: { xs: "auto", sm: 2 } }} item>
+						{artist ? (
+							<Card sx={{ borderRadius: 5 }} onClick={handleImageOpen}>
+								<CardActionArea>
+									<CardMedia
+										component="img"
+										width={200}
+										height={200}
+										image={artist.images[0]?.url || ""}
+										alt="Image"
+									/>
+								</CardActionArea>
+							</Card>
+						) : (
+							<Skeleton
+								sx={{ borderRadius: 5 }}
+								variant="rectangular"
+								width={200}
+								height={200}
+							/>
+						)}
+					</Grid>
+					<Grid
+						sx={{
+							my: { xs: 1, sm: 3 },
+							mx: { xs: "auto", sm: 3 },
+							textAlign: { xs: "center", sm: "start" }
+						}}
+						item
+						display="flex"
+						flexDirection="column"
+						justifyContent="center">
+						{artist ? (
+							<>
+								<Typography variant="h4">{artist.name}</Typography>
+								<Typography variant="h5">{getFollowers(artist)}</Typography>
+							</>
+						) : (
+							<>
+								<Skeleton variant="text" width={200} height={45} />
+								<Skeleton variant="text" width={160} height={40} />
+							</>
+						)}
+
+						<Tooltip title="Open in Spotify">
+							<IconButton
+								sx={{ width: 46, mx: { xs: "auto", sm: 0 } }}
+								onClick={handleArtistOpen}>
+								<Avatar
+									sx={{ width: 30, height: 30 }}
+									src="/assets/spotify-logo.png"
+									alt="Spotify"
+								/>
+							</IconButton>
+						</Tooltip>
+					</Grid>
+				</Grid>
+
+				<Typography sx={{ mt: 2 }} variant="h5">
+					{artist ? artist.name + "'s " : ""}Top Tracks
+				</Typography>
+				<List>
+					{topTracks.map((track, i) => (
+						<Card sx={{ my: 1 }} key={track.id} onClick={() => handleTrackClick(track)}>
+							<CardActionArea>
+								<ListItem>
+									<ListItemAvatar>
+										<Avatar
+											sx={{ width: 45, height: 45 }}
+											src={track.album.images.at(-1)?.url || ""}
+										/>
+									</ListItemAvatar>
+									<ListItemText
+										primary={i + 1 + ". " + track.name}
+										secondary={track.artists.map(a => a.name).join(", ")}
+									/>
+								</ListItem>
+							</CardActionArea>
+						</Card>
+					))}
+				</List>
+
+				<ArtistAppearances artist={artist || undefined} />
+				<Recommendations artist={artist || undefined} />
+			</Container>
+			<Dialog open={showImage} onClose={handleImageClose} BackdropComponent={Backdrop}>
+				<Box
+					sx={{ width: { xs: 300, sm: 500 }, height: { xs: 300, sm: 500 } }}
+					component="img"
+					src={artist?.images[0]?.url || ""}
+					alt="Image"
+				/>
+			</Dialog>
+		</>
+	)
 }
 
 export default Artist
