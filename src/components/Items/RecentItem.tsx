e8ec3580a457eb1be169854e97c0462b20a0c20e
Zechariah <zechariahtan144@gmail.com>
2022-03-17 21:11:30 +0800

    Removed lazy loading for tables

--- src/components/Items/RecentItem.tsx
+++ src/components/Items/RecentItem.tsx
@@ -39,44 +39,11 @@ const RecentItem: React.FC<Props> = (props: Props) => {
 		navigate("/artist/" + artist.id)
 	}
 
-	return (
+	return smallScreen ? (
 		<LazyLoad height={72}>
-			{smallScreen ? (
-				<ListItem onClick={() => handleTrackClick(recent?.track)} disablePadding>
-					<ListItemButton>
-						<ListItemAvatar>
-							<AsyncImage
-								src={image}
-								skeleton={<Skeleton variant="circular" width={45} height={45} />}
-								component={thumbnailUrl => (
-									<Avatar sx={{ width: 45, height: 45 }} src={thumbnailUrl} />
-								)}
-							/>
-						</ListItemAvatar>
-						{recent ? (
-							<ListItemText
-								primary={
-									recent.track.name +
-									" - " +
-									recent.track.artists.map(a => a.name).join(", ")
-								}
-								secondary={
-									getTimeSincePlayed(recent) +
-									" ago, " +
-									DateTime.fromISO(recent.played_at).toFormat("d LLLL yyyy")
-								}
-							/>
-						) : (
-							<Stack my="6px">
-								<Skeleton variant="text" width={200} height={24} />
-								<Skeleton variant="text" width={160} height={20} />
-							</Stack>
-						)}
-					</ListItemButton>
-				</ListItem>
-			) : (
-				<TableRow hover>
-					<TableCell>
+			<ListItem onClick={() => handleTrackClick(recent?.track)} disablePadding>
+				<ListItemButton>
+					<ListItemAvatar>
 						<AsyncImage
 							src={image}
 							skeleton={<Skeleton variant="circular" width={45} height={45} />}
@@ -84,62 +51,90 @@ const RecentItem: React.FC<Props> = (props: Props) => {
 								<Avatar sx={{ width: 45, height: 45 }} src={thumbnailUrl} />
 							)}
 						/>
-					</TableCell>
-					<TableCell>
-						{recent ? (
-							<Typography variant="body1">
-								<Link
-									sx={{ cursor: "pointer" }}
-									color="inherit"
-									onClick={() => handleTrackClick(recent.track)}
-									underline="hover">
-									{recent.track.name}
-								</Link>
-							</Typography>
-						) : (
+					</ListItemAvatar>
+					{recent ? (
+						<ListItemText
+							primary={
+								recent.track.name +
+								" - " +
+								recent.track.artists.map(a => a.name).join(", ")
+							}
+							secondary={
+								getTimeSincePlayed(recent) +
+								" ago, " +
+								DateTime.fromISO(recent.played_at).toFormat("d LLLL yyyy")
+							}
+						/>
+					) : (
+						<Stack my="6px">
+							<Skeleton variant="text" width={200} height={24} />
 							<Skeleton variant="text" width={160} height={20} />
-						)}
-					</TableCell>
-					<TableCell>
-						{recent ? (
-							<Typography variant="body1">
-								{recent.track.artists
-									.map(artist => (
-										<Link
-											sx={{
-												cursor: "pointer"
-											}}
-											key={artist.id}
-											color="inherit"
-											onClick={() => handleArtistClick(artist)}
-											underline="hover">
-											{artist.name}
-										</Link>
-									))
-									.reduce<(JSX.Element | string)[]>(
-										(r, a) => r.concat(a, ", "),
-										[]
-									)
-									.slice(0, -1)}
-							</Typography>
-						) : (
-							<Skeleton variant="text" width={120} height={20} />
-						)}
-					</TableCell>
-					<TableCell align="center">
-						{recent ? (
-							<Typography variant="body1">
-								{getTimeSincePlayed(recent) +
-									" ago, " +
-									DateTime.fromISO(recent.played_at).toFormat("d LLLL yyyy")}
-							</Typography>
-						) : (
-							<Skeleton variant="text" width={160} height={20} />
-						)}
-					</TableCell>
-				</TableRow>
-			)}
+						</Stack>
+					)}
+				</ListItemButton>
+			</ListItem>
 		</LazyLoad>
+	) : (
+		<TableRow hover>
+			<TableCell>
+				<AsyncImage
+					src={image}
+					skeleton={<Skeleton variant="circular" width={45} height={45} />}
+					component={thumbnailUrl => (
+						<Avatar sx={{ width: 45, height: 45 }} src={thumbnailUrl} />
+					)}
+				/>
+			</TableCell>
+			<TableCell>
+				{recent ? (
+					<Typography variant="body1">
+						<Link
+							sx={{ cursor: "pointer" }}
+							color="inherit"
+							onClick={() => handleTrackClick(recent.track)}
+							underline="hover">
+							{recent.track.name}
+						</Link>
+					</Typography>
+				) : (
+					<Skeleton variant="text" width={160} height={20} />
+				)}
+			</TableCell>
+			<TableCell>
+				{recent ? (
+					<Typography variant="body1">
+						{recent.track.artists
+							.map(artist => (
+								<Link
+									sx={{
+										cursor: "pointer"
+									}}
+									key={artist.id}
+									color="inherit"
+									onClick={() => handleArtistClick(artist)}
+									underline="hover">
+									{artist.name}
+								</Link>
+							))
+							.reduce<(JSX.Element | string)[]>((r, a) => r.concat(a, ", "), [])
+							.slice(0, -1)}
+					</Typography>
+				) : (
+					<Skeleton variant="text" width={120} height={20} />
+				)}
+			</TableCell>
+			<TableCell align="center">
+				{recent ? (
+					<Typography variant="body1">
+						{getTimeSincePlayed(recent) +
+							" ago, " +
+							DateTime.fromISO(recent.played_at).toFormat("d LLLL yyyy")}
+					</Typography>
+				) : (
+					<Skeleton variant="text" width={160} height={20} />
+				)}
+			</TableCell>
+		</TableRow>
 	)
 }
 
--- src/components/Items/TopTrackItem.tsx
+++ src/components/Items/TopTrackItem.tsx
@@ -40,37 +40,11 @@ const TopTrackItem: React.FC<Props> = (props: Props) => {
 		}
 	}
 
-	return (
+	return smallScreen ? (
 		<LazyLoad height={72}>
-			{smallScreen ? (
-				<ListItem onClick={() => handleTrackClick(track)} disablePadding>
-					<ListItemButton>
-						<ListItemAvatar>
-							<AsyncImage
-								src={track?.album.images[0]?.url}
-								skeleton={<Skeleton variant="circular" width={45} height={45} />}
-								component={thumbnailUrl => (
-									<Avatar sx={{ width: 45, height: 45 }} src={thumbnailUrl} />
-								)}
-							/>
-						</ListItemAvatar>
-						{track ? (
-							<ListItemText
-								primary={(i !== undefined ? i + 1 + ". " : "") + track.name}
-								secondary={track.artists.map(a => a.name).join(", ")}
-							/>
-						) : (
-							<Stack my="6px">
-								<Skeleton variant="text" width={200} height={24} />
-								<Skeleton variant="text" width={160} height={20} />
-							</Stack>
-						)}
-					</ListItemButton>
-				</ListItem>
-			) : (
-				<TableRow hover>
-					<TableCell align="center">{i! + 1}</TableCell>
-					<TableCell>
+			<ListItem onClick={() => handleTrackClick(track)} disablePadding>
+				<ListItemButton>
+					<ListItemAvatar>
 						<AsyncImage
 							src={track?.album.images[0]?.url}
 							skeleton={<Skeleton variant="circular" width={45} height={45} />}
@@ -78,60 +52,79 @@ const TopTrackItem: React.FC<Props> = (props: Props) => {
 								<Avatar sx={{ width: 45, height: 45 }} src={thumbnailUrl} />
 							)}
 						/>
-					</TableCell>
-					<TableCell>
-						{track ? (
-							<Typography variant="body1">
-								<Link
-									sx={{ cursor: "pointer" }}
-									color="inherit"
-									onClick={() => handleTrackClick(track)}
-									underline="hover">
-									{track.name}
-								</Link>
-							</Typography>
-						) : (
+					</ListItemAvatar>
+					{track ? (
+						<ListItemText
+							primary={(i !== undefined ? i + 1 + ". " : "") + track.name}
+							secondary={track.artists.map(a => a.name).join(", ")}
+						/>
+					) : (
+						<Stack my="6px">
+							<Skeleton variant="text" width={200} height={24} />
 							<Skeleton variant="text" width={160} height={20} />
-						)}
-					</TableCell>
-					<TableCell>
-						{track ? (
-							<Typography variant="body1">
-								{track.artists
-									.map(artist => (
-										<Link
-											sx={{
-												cursor: "pointer"
-											}}
-											key={artist.id}
-											color="inherit"
-											onClick={() => handleArtistClick(artist)}
-											underline="hover">
-											{artist.name}
-										</Link>
-									))
-									.reduce<(JSX.Element | string)[]>(
-										(r, a) => r.concat(a, ", "),
-										[]
-									)
-									.slice(0, -1)}
-							</Typography>
-						) : (
-							<Skeleton variant="text" width={120} height={20} />
-						)}
-					</TableCell>
-					<TableCell align="center">
-						{track ? (
-							<Typography variant="body1">
-								{getDuration(track.duration_ms)}
-							</Typography>
-						) : (
-							<Skeleton variant="text" width={160} height={20} />
-						)}
-					</TableCell>
-				</TableRow>
-			)}
+						</Stack>
+					)}
+				</ListItemButton>
+			</ListItem>
 		</LazyLoad>
+	) : (
+		<TableRow hover>
+			<TableCell align="center">{i! + 1}</TableCell>
+			<TableCell>
+				<AsyncImage
+					src={track?.album.images[0]?.url}
+					skeleton={<Skeleton variant="circular" width={45} height={45} />}
+					component={thumbnailUrl => (
+						<Avatar sx={{ width: 45, height: 45 }} src={thumbnailUrl} />
+					)}
+				/>
+			</TableCell>
+			<TableCell>
+				{track ? (
+					<Typography variant="body1">
+						<Link
+							sx={{ cursor: "pointer" }}
+							color="inherit"
+							onClick={() => handleTrackClick(track)}
+							underline="hover">
+							{track.name}
+						</Link>
+					</Typography>
+				) : (
+					<Skeleton variant="text" width={160} height={20} />
+				)}
+			</TableCell>
+			<TableCell>
+				{track ? (
+					<Typography variant="body1">
+						{track.artists
+							.map(artist => (
+								<Link
+									sx={{
+										cursor: "pointer"
+									}}
+									key={artist.id}
+									color="inherit"
+									onClick={() => handleArtistClick(artist)}
+									underline="hover">
+									{artist.name}
+								</Link>
+							))
+							.reduce<(JSX.Element | string)[]>((r, a) => r.concat(a, ", "), [])
+							.slice(0, -1)}
+					</Typography>
+				) : (
+					<Skeleton variant="text" width={120} height={20} />
+				)}
+			</TableCell>
+			<TableCell align="center">
+				{track ? (
+					<Typography variant="body1">{getDuration(track.duration_ms)}</Typography>
+				) : (
+					<Skeleton variant="text" width={160} height={20} />
+				)}
+			</TableCell>
+		</TableRow>
 	)
 }
 
