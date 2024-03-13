e99e3f4c8aef21c7d7b3ac4cfe581e3331d6e9c6
zS1L3NT Mac <zechariahtan144@gmail.com>
2024-03-13 09:51:07 +0800

    🔥 nuke unused files and dependencies

--- src/hooks/useAppDispatch.ts
+++
@@ -1,6 +0,0 @@
-import { useDispatch } from "react-redux"
-
-import { AppDispatch } from "../store"
-
-const useAppDispatch = (): AppDispatch => useDispatch<AppDispatch>()
-export default useAppDispatch
--- src/hooks/useThemeValue.ts
+++
@@ -1,9 +0,0 @@
-import useAppSelector from "./useAppSelector"
-
-const useThemeValue = <T>(dark: T, light: T): T => {
-	const theme = useAppSelector(state => state.theme)
-
-	return theme === "dark" ? dark : light
-}
-
-export default useThemeValue
