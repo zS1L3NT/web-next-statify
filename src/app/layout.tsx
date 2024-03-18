c88f38b73bb2f494f4f9a2e2c1173b4893fbf615
zS1L3NT Mac <zechariahtan144@gmail.com>
2024-03-18 23:21:04 +0800

    🔥 remove all previous statify code in home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,13 @@
 import { PropsWithChildren } from "react"
 
-import Navbar from "@/components/Navbar/Navbar"
-import theme from "@/theme"
-
-import "../global.css"
+import "./globals.scss"
 
 export const revalidate = 15 * 60
 
 export default function RootLayout({ children }: PropsWithChildren) {
 	return (
 		<html lang="en">
-			<body>
-				<AppRouterCacheProvider>
-					<ThemeProvider theme={theme}>
-						<CssBaseline />
-						<Navbar />
-						{children}
-					</ThemeProvider>
-				</AppRouterCacheProvider>
-			</body>
+			<body>{children}</body>
 		</html>
 	)
 }
